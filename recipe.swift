dea0938c46b823f043a27384ff440406d7f7196b
cmaker-beep <cmaker@saac.qld.edu.au>
2026-07-20 13:20:49 +1000

    Create recipe.swift

---
+++ recipe.swift
@@ -0,0 +1,16 @@
+//
+//  recipe.swift
+//  RecipeV1
+//
+//  Created by Chris Maker on 20/7/2026.
+//
+
+import SwiftUI
+import FirebaseFirestore
+
+
+struct Recipe {
+    var title: String
+    var ingredients: [String]
+}
+
